8e174c008d7078c2b1e9d853b85507b5eab09948
Ryo Kato <katoryo55@gmail.com>
2015-12-05 16:18:48 +0900

    Implement basic classes

--- Pod/Classes/AudioLoader.swift
+++ Pod/Classes/AudioLoader.swift
@@ -6,4 +6,131 @@
 //
 //
 
-import Foundation
+import AVFoundation
+import MobileCoreServices
+
+class AudioLoader: NSObject, AVAssetResourceLoaderDelegate, NSURLConnectionDataDelegate {
+    var pendingRequests = [AVAssetResourceLoadingRequest]()
+    var songData = NSMutableData()
+    var response: NSURLResponse?
+    var connections = [String: NSURLConnection]()
+    var audioCache = SlashAcceptableCache?()
+    
+    
+    init(cache: SlashAcceptableCache) {
+        audioCache = cache
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        self.songData = NSMutableData()
+        self.response = response
+        self.processPendingRequests()
+    }
+    
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        self.songData.appendData(data)
+        self.processPendingRequests()
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        self.processPendingRequests()
+        let url = getActualURL(connection.currentRequest.URL!)
+        let urlString = url.absoluteString
+        if (audioCache!.objectForKey(urlString) != nil) {
+            return
+        }
+        audioCache!.setObjectForKey(songData, key: urlString)
+    }
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        print(error)
+    }
+    
+    func resourceLoader(resourceLoader: AVAssetResourceLoader, shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        let interceptedURL = loadingRequest.request.URL
+        let actualURL = getActualURL(interceptedURL!)
+        let urlString = actualURL.absoluteString
+        if (connections[urlString] == nil) {
+            let request = NSURLRequest(URL: actualURL)
+            let connection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+            connection.setDelegateQueue(NSOperationQueue.mainQueue())
+            connection.start()
+            connections[actualURL.absoluteString] = connection
+        }
+        self.pendingRequests.append(loadingRequest)
+        return true
+    }
+    
+    func resourceLoader(resourceLoader: AVAssetResourceLoader, didCancelLoadingRequest loadingRequest: AVAssetResourceLoadingRequest) {
+        pendingRequests = pendingRequests.filter({ $0 != loadingRequest })
+    }
+    
+    private func processPendingRequests() {
+        var requestsCompleted = [AVAssetResourceLoadingRequest]()
+        for loadingRequest in pendingRequests {
+            fillInContentInformation(loadingRequest.contentInformationRequest)
+            let didRespondCompletely = respondWithDataForRequest(loadingRequest.dataRequest!)
+            if didRespondCompletely == true {
+                requestsCompleted.append(loadingRequest)
+                loadingRequest.finishLoading()
+            }
+        }
+        for requestCompleted in requestsCompleted {
+            for (i, pendingRequest) in pendingRequests.enumerate() {
+                if requestCompleted == pendingRequest {
+                    pendingRequests.removeAtIndex(i)
+                }
+            }
+        }
+    }
+    
+    private func fillInContentInformation(contentInformationRequest: AVAssetResourceLoadingContentInformationRequest?) {
+        if(contentInformationRequest == nil) {
+            return
+        }
+        if (self.response == nil) {
+            return
+        }
+        
+        let mimeType = self.response!.MIMEType
+        let unmanagedContentType = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, mimeType!, nil)
+        let cfContentType = unmanagedContentType!.takeRetainedValue()
+        contentInformationRequest!.contentType = String(cfContentType)
+        contentInformationRequest!.byteRangeAccessSupported = true
+        contentInformationRequest!.contentLength = self.response!.expectedContentLength
+    }
+    
+    private func respondWithDataForRequest(dataRequest: AVAssetResourceLoadingDataRequest) -> Bool {
+        var startOffset = dataRequest.requestedOffset
+        if dataRequest.currentOffset != 0 {
+            startOffset = dataRequest.currentOffset
+        }
+        let songDataLength = Int64(self.songData.length)
+        if songDataLength < startOffset {
+            return false
+        }
+        let unreadBytes = songDataLength - startOffset
+        let numberOfBytesToRespondWith: Int64
+        if Int64(dataRequest.requestedLength) > unreadBytes {
+            numberOfBytesToRespondWith = unreadBytes
+        } else {
+            numberOfBytesToRespondWith = Int64(dataRequest.requestedLength)
+        }
+        dataRequest.respondWithData(self.songData.subdataWithRange(NSMakeRange(Int(startOffset), Int(numberOfBytesToRespondWith))))
+        let endOffset = startOffset + dataRequest.requestedLength
+        let didRespondFully = songDataLength >= endOffset
+        return didRespondFully
+    }
+    
+    private func getActualURL(url: NSURL) -> NSURL {
+        let actualURLComponents = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)
+        if url.scheme == "httpstreaming" {
+            actualURLComponents!.scheme = "http"
+        } else if url.scheme == "httpsstreaming" {
+            actualURLComponents!.scheme = "https"
+        }
+        print("actualURLCoponents:" + actualURLComponents!.URL!.absoluteString)
+        return actualURLComponents!.URL!
+    }
+    
+}
--- Pod/Classes/StreamingAudioCacheContainer.swift
+++ Pod/Classes/StreamingAudioCacheContainer.swift
@@ -6,4 +6,95 @@
 //
 //
 
-import Foundation
+
+//import UIKit
+import AVFoundation
+import AwesomeCache
+
+var context = "playAudioContext"
+
+class StreamingAudioCacheContainer: NSObject {
+    var musicPlayer: AVPlayer = AVPlayer()
+    var musicPlayerItems = [AVPlayerItem]()
+    var isLooping: Bool
+    var audioLoader: AudioLoader
+    var audioCache = SlashAcceptableCache?()
+    
+    init (repeats: Bool) {
+        var audioCacheBody = Cache<NSData>?()
+        do {
+            audioCacheBody = try Cache<NSData>(name: "audio")
+        } catch _ {
+            print("Failed to initialize audioCache")
+        }
+        audioCache = SlashAcceptableCache(cache: audioCacheBody!)
+        self.isLooping = repeats
+        audioLoader = AudioLoader(cache: audioCache!)
+        super.init()
+    }
+    
+    deinit {
+        for item in musicPlayerItems {
+            item.removeObserver(self, forKeyPath: "status")
+        }
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func reset() {
+        audioCache!.removeAllObjects()
+        audioLoader = AudioLoader(cache: audioCache!)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if musicPlayer.currentItem!.status == AVPlayerItemStatus.ReadyToPlay {
+            if keyPath == "status" {
+                musicPlayer.play()
+                if isLooping {
+                    NSNotificationCenter.defaultCenter().addObserver(self, selector: "musicFinished", name: AVPlayerItemDidPlayToEndTimeNotification, object: musicPlayer.currentItem)
+                    
+                }
+                
+            }
+        }
+    }
+    
+    func musicFinished() {
+        musicPlayer.seekToTime(CMTimeMake(0, 600))
+        musicPlayer.play()
+    }
+    
+    
+    func changeAudio(url: NSURL) {
+        let asset = loadAssetFromCacheOrWeb(url)
+        let playerItem = AVPlayerItem(asset: asset)
+        musicPlayer.replaceCurrentItemWithPlayerItem(playerItem)
+        playerItem.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions.New, context: &context)
+        musicPlayerItems.append(playerItem)
+    }
+    
+    func pauseAudio() {
+        musicPlayer.pause()
+    }
+    
+    private func loadAssetFromCacheOrWeb(url: NSURL) -> AVURLAsset {
+        let urlString = url.absoluteString
+        var asset: AVURLAsset
+        if (audioCache?.objectForKey(urlString) != nil) {
+            let path = audioCache!.pathForKey(urlString)
+            let filePathURL = NSURL.fileURLWithPath(path)
+            asset = AVURLAsset(URL: filePathURL, options: nil)
+        } else {
+            print("Audio resource \(url) not found in audioCache.")
+            asset = AVURLAsset(URL: url, options: nil)
+        }
+        asset.resourceLoader.setDelegate(audioLoader, queue: dispatch_get_main_queue())
+        return asset
+    }
+    
+    private func URLWithCustomScheme(url: NSURL, scheme: String) -> NSURL {
+        let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)!
+        components.scheme = scheme
+        return components.URL!
+    }
+}
+
